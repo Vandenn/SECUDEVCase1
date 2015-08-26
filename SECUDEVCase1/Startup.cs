7215f7f1165d8cd386a048b5ca2e2efb73ded1ff
Vandenn <evan.livelo@gmail.com>
2015-08-26 14:39:50 +0800

    SECUDEV Case 1 Initial Push

---
+++ SECUDEVCase1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SECUDEVCase1.Startup))]
+namespace SECUDEVCase1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
